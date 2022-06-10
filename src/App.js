d6a246acc4c5a4333a4f277955842e404f8696f5
mikami koji <wmd147wmd@gmail.com>
2022-06-11 00:13:53 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import SearchBar from "./components/SearchBar";
+import ImageList from "./components/ImageList";
+import pixabayLogo from "./pixabayLogo.png";
 
-function App() {
+const App = () => {
+  const [images, setImages] = useState([]);
+  const ApiKey = process.env.REACT_APP_PIXABAY_APIKEY;
+  const onSearchSubmit = async (term) => {
+    try {
+      const params = {
+        key: ApiKey,
+        q: term,
+        per_page: 40,
+      };
+      const response = await axios.get("https://pixabay.com/api/", { params });
+      setImages(response.data.hits);
+      if (response.data.total === 0) {
+        window.alert("お探しの画像はありません。");
+      }
+    } catch (error) {
+      window.alert("画像の取得に失敗しました。");
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="ui container" style={{ marginTop: "20px" }}>
+      <img src={pixabayLogo} alt="pixabay-logo" className="pixabay-logo" />
+      <SearchBar onSubmit={onSearchSubmit} />
+      <ImageList images={images} />
     </div>
   );
-}
+};
 
 export default App;
